b1500d7ad73e4570d1a8a0d82891e0d75c5f3501
Eikcaj.1998 <jw220998@gmail.com>
2022-06-21 15:09:13 +0200

    under 18

--- js/script.js
+++ js/script.js
@@ -18,11 +18,26 @@ const kmNumber = parseInt ( prompt ('Inserisci qui il numero dei chilometri che 
 const kmPrice = 0.21;
 let ticketPrice = kmNumber * kmPrice;
     //Age
-const passengerAge = parseInt ( prompt ('Inserisci qui il l\'età','20'));
+const passengerAge = parseInt ( prompt ('Inserisci qui il l\'età','23'));
+let message = `Il costo del biglietto e di `;
 if (passengerAge<18 && passengerAge>65){
+     //minore di 18 anni
+    if(passengerAge < 18){
+        //sconto 20%
+        let discount = ((ticketPrice * 20) / 100);
+        console.log(discount)
+        //totale - sconto
+        let discountedTicketPrice = ticketPrice - discount;
+        console.log(discountedTicketPrice)
+        // prezzo finale
+        let discountedPrice = discountedTicketPrice.toFixed(2);
+        console.log(discountedPrice) 
+        message = message + discountedPrice
+    }
+        //maggiore di 18 anni ma minore di 65 anni
+}else {
+  console.log (ticketPrice)
+  message = message + ticketPrice
+}
 
-} else {
-  console.log (ticketPrice)}
-
-let message = `Il costo del biglietto e di <b> ${ticketPrice} &euro </b>`;
 console.log(message);

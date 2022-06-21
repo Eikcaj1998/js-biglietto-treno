11fe3408e0c1afc43b8cb7b7b4b36e040f2b6927
Eikcaj.1998 <jw220998@gmail.com>
2022-06-21 15:45:27 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -15,29 +15,44 @@ inserisco il costo con lo sconto
  */
     //Km
 const kmNumber = parseInt ( prompt ('Inserisci qui il numero dei chilometri che farai','20'));
+console.log(kmNumber)
 const kmPrice = 0.21;
+console.log(kmPrice)
 let ticketPrice = kmNumber * kmPrice;
+console.log(ticketPrice)
     //Age
-const passengerAge = parseInt ( prompt ('Inserisci qui il l\'età','23'));
+const passengerAge = parseInt ( prompt ('Inserisci qui il l\'età','25'));
+console.log(passengerAge)
 let message = `Il costo del biglietto e di `;
-if (passengerAge<18 && passengerAge>65){
+let ticketAge =  `sconto di `;
+if (passengerAge < 18 || passengerAge > 65){
      //minore di 18 anni
     if(passengerAge < 18){
         //sconto 20%
         let discount = ((ticketPrice * 20) / 100);
-        console.log(discount)
+        console.log(ticketAge + discount)
         //totale - sconto
         let discountedTicketPrice = ticketPrice - discount;
-        console.log(discountedTicketPrice)
-        // prezzo finale
         let discountedPrice = discountedTicketPrice.toFixed(2);
         console.log(discountedPrice) 
+                // prezzo finale
         message = message + discountedPrice
-    }
+         //minore di 65 anni
+    } else if (passengerAge > 65) {
+         //sconto 40%
+    let discount = ((ticketPrice * 40) / 100);
+    console.log(ticketAge + discount)
+        //totale - sconto
+    let discountedTicketPrice = ticketPrice - discount;
+    let discountedPrice = discountedTicketPrice.toFixed(2);
+    console.log(discountedPrice)
+    // prezzo finale
+    message = message + discountedPrice
+        }   
         //maggiore di 18 anni ma minore di 65 anni
 }else {
-  console.log (ticketPrice)
-  message = message + ticketPrice
+    let basePrice = ticketPrice.toFixed(2);
+    console.log(basePrice);
+    message = message + basePrice
 }
-
 console.log(message);
